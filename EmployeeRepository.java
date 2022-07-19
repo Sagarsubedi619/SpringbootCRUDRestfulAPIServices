4d91819d59983f9ec151aeed7391431ca9ad59d5
Sagar Subedi <59858676+Sagarsubedi619@users.noreply.github.com>
2022-07-19 12:03:58 -0500

    Update EmployeeRepository.java

--- EmployeeRepository.java
+++ EmployeeRepository.java
@@ -1,5 +1,12 @@
 package net.javaspringrestful.springboot.crudrestfulwebservices.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaspringrestful.springboot.crudrestfulwebservices.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+	
 
 }
