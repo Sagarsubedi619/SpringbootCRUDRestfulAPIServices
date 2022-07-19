535996624262043cd87cded0880599e050c8fd83
Sagar Subedi <59858676+Sagarsubedi619@users.noreply.github.com>
2022-07-19 12:01:53 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,5 +1,58 @@
 package net.javaspringrestful.springboot.crudrestfulwebservices.model;
 
-public class Employee {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table
+public class Employee {
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="email_id")
+	private String emailId;
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	
+	public Employee() {
+		super();
+	}
+	public Employee(String firstName, String lastName, String emailId, long id) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.id = id;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
 }
+
